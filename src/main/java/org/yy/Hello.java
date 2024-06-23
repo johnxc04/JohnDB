d206021fb4bae4744235c37f4f9a72ea4718126d
Spreps <1410376465@qq.com>
2024-06-24 02:19:07 +0800

    Revert "更改"
    This reverts commit 85f896e14fd0acab070c0755df62275bb2513234.

--- src/main/java/org/yy/Hello.java
+++ src/main/java/org/yy/Hello.java
@@ -3,7 +3,5 @@ package org.yy;
 public class Hello {
     public void test() {
         System.out.println("hello!");
-        System.out.println("I am hy");
-        System.out.println("nihao");
     }
 }
