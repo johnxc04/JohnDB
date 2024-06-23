a55eb3b5cda9b40b721a4167b9c26c86397688f5
HuangYuan1024 <3205518128@qq.com>
2024-06-23 23:28:54 +0800

    测试

--- src/main/java/org/yy/Main.java
+++ src/main/java/org/yy/Main.java
@@ -2,6 +2,6 @@ package org.yy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello YYDB!");
+        System.out.println("Hello, YYDB!");
     }
 }
