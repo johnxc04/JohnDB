bffc8dcb8edc6d36c21122bf9c26969d73a1a2c6
gitInit <johnxc04@163.com>
2024-07-03 17:12:29 +0800

    完成了压缩算法并删除了冗余代码

--- src/main/java/org/yy/service/NormalStore.java
+++ src/main/java/org/yy/service/NormalStore.java
@@ -72,6 +72,7 @@ public class NormalStore implements Store {
             LoggerUtil.info(LOGGER,logFormat, "NormalStore","dataDir isn't exist,creating...");
             file.mkdirs();
         }
+        this.reloadIndex();
         this.RotateDataBaseFile();
     }
 
@@ -79,9 +80,47 @@ public class NormalStore implements Store {
         return this.dataDir + File.separator + NAME + TABLE;
     }
 
+    public String genNewFilePath() {
+        return this.dataDir + File.separator + NAME + "0" + TABLE;
+    }
+
+    public String genScratchFilePath() {
+        return this.dataDir + File.separator + "Scratch.log";
+    }
+
     public void reloadIndex() {
         try {
             RandomAccessFile file = new RandomAccessFile(this.genFilePath(), RW_MODE);
+            long len = file.length();
+            long start = 0;
+            file.seek(start);
+            while (start < len) {
+                int cmdLen = file.readInt();
+                byte[] bytes = new byte[cmdLen];
+                file.read(bytes);
+                JSONObject value = JSON.parseObject(new String(bytes, StandardCharsets.UTF_8));
+                Command command = CommandUtil.jsonToCommand(value);
+                start += 4;
+                if (command != null) {
+                    CommandPos cmdPos = new CommandPos((int) start, cmdLen);
+                    index.put(command.getKey(), cmdPos);
+                    // 如果日志中记载该键值是被删除的，就将其从内存里删去
+                    if (command.getClass().equals(RmCommand.class)) {
+                        index.remove(command.getKey(), cmdPos);
+                    }
+                }
+                start += cmdLen;
+            }
+            file.seek(file.length());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        LoggerUtil.debug(LOGGER, logFormat, "reload index: "+index.toString());
+    }
+
+    public void reloadScratchIndex() {
+        try {
+            RandomAccessFile file = new RandomAccessFile(this.genScratchFilePath(), RW_MODE);
             long len = file.length();
             long start = 0;
             file.seek(start);
@@ -125,6 +164,7 @@ public class NormalStore implements Store {
             // 写table（wal）文件
             RandomAccessFileUtil.writeInt(this.genFilePath(), commandBytes.length);
             int pos = RandomAccessFileUtil.write(this.genFilePath(), commandBytes);
+            // 保存到memTable
             // 添加索引
             CommandPos cmdPos = new CommandPos(pos, commandBytes.length);
             index.put(key, cmdPos);
@@ -133,11 +173,6 @@ public class NormalStore implements Store {
             throw new RuntimeException(t);
         } finally {
             indexLock.writeLock().unlock();
-        }
-        try {
-            FilCompress(new File(this.genFilePath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 
@@ -151,6 +186,38 @@ public class NormalStore implements Store {
                 return null;
             }
             byte[] commandBytes = RandomAccessFileUtil.readByIndex(this.genFilePath(), cmdPos.getPos(), cmdPos.getLen());
+
+            JSONObject value = null;
+            if (commandBytes != null) {
+                value = JSONObject.parseObject(new String(commandBytes));
+            }
+            Command cmd = null;
+            if (value != null) {
+                cmd = CommandUtil.jsonToCommand(value);
+            }
+            if (cmd instanceof SetCommand) {
+                return ((SetCommand) cmd).getValue();
+            }
+            if (cmd instanceof RmCommand) {
+                return null;
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.readLock().unlock();
+        }
+        return null;
+    }
+
+    public String ReGet(String key) {
+        try {
+            indexLock.readLock().lock();
+            // 从索引中获取信息
+            CommandPos cmdPos = index.get(key);
+            if (cmdPos == null) {
+                return null;
+            }
+            byte[] commandBytes = RandomAccessFileUtil.readByIndex(genScratchFilePath(), cmdPos.getPos(), cmdPos.getLen());
 
             JSONObject value = null;
             if (commandBytes != null) {
@@ -199,17 +266,11 @@ public class NormalStore implements Store {
         } finally {
             indexLock.writeLock().unlock();
         }
-        try {
-            FilCompress(new File(this.genFilePath()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
     public void ReDoLog() {
         reloadIndex();
-
         RotateDataBaseFile();
     }
 
@@ -224,91 +285,63 @@ public class NormalStore implements Store {
         }
     }
 
-    public void CompressIndexFile() {
-        ArrayList<String> arrayList = new ArrayList<>();
-        HashSet<String> hashSet = new HashSet<>();
+    public void ScratchLogSet(String key, String value) {
+        try {
+            SetCommand command = new SetCommand(key, value);
+            byte[] commandBytes = command.toByte();
+            // 加锁
+            indexLock.writeLock().lock();
+            // 写table（wal）文件
+            RandomAccessFileUtil.writeInt(genScratchFilePath(), commandBytes.length);
+            RandomAccessFileUtil.write(genScratchFilePath(), commandBytes);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.writeLock().unlock();
+        }
+    }
 
-        try (Scanner scanner = new Scanner(new File(this.genFilePath()))) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                // 处理每一行
-                if (!hashSet.contains(line)) {
-                    arrayList.add(line);
-                    hashSet.add(line);
-                } else if (arrayList.size() > 1){
-                    if (!arrayList.get(arrayList.size() - 1).equals(line)) {
-                        arrayList.add(line);
-                    }
-                }
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    public void ReSet(String key, String value) {
+        try {
+            SetCommand command = new SetCommand(key, value);
+            byte[] commandBytes = command.toByte();
+            // 加锁
+            indexLock.writeLock().lock();
+            // 写table（wal）文件
+            RandomAccessFileUtil.writeInt(genFilePath(), commandBytes.length);
+            RandomAccessFileUtil.write(genFilePath(), commandBytes);
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        } finally {
+            indexLock.writeLock().unlock();
+        }
+    }
+
+    public void CompressLogFile() {
+        // 索引压缩后写到临时文件
+        for (String key : index.keySet()) {
+            ScratchLogSet(key, Get(key));
         }
 
-        ClearDataBaseFile(this.genFilePath());
+        // 从临时文件加载索引
+        reloadScratchIndex();
 
-        try (FileWriter writer = new FileWriter(this.genFilePath())) {
-            for (String line :
-                arrayList) {
-                writer.write(line + "\r\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        ClearDataBaseFile(genFilePath());
+
+        // 重写日志文件
+        for (String key : index.keySet()) {
+            ReSet(key, ReGet(key));
         }
 
-        index.clear();
+        ClearDataBaseFile(genScratchFilePath());
 
         reloadIndex();
     }
 
     public void RotateDataBaseFile() {
-        // 清空数据库文件
-        ClearDataBaseFile("YY-table");
 
         // 压缩日志文件
-        CompressIndexFile();
+        CompressLogFile();
 
-        // 重写数据库文件
-        try (FileWriter writer = new FileWriter("YY-table")) {
-            for (String key : index.keySet()) {
-                writer.write(key + "," + Get(key) + "\r\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void FilCompress(File file) throws IOException {
-        FileInputStream fisp = new FileInputStream("p.properties");
-        Properties prop = new Properties();
-        prop.load(fisp);
-        fisp.close();
-        int MAXFILELENTH = Integer.parseInt(prop.getProperty("MAXFILELENTH"));
-        int TIMES = Integer.parseInt(prop.getProperty("TIMES"));
-        if(file.length() > MAXFILELENTH){
-            TIMES++;
-            prop.put("TIMES",Integer.toString(TIMES));
-            FileOutputStream fosp = new FileOutputStream("p.properties");
-            prop.store(fosp, null);
-            fosp.close();
-            new FileOutputStream(file.getAbsolutePath() + TIMES);
-//            try(FileInputStream fis = new FileInputStream(file);
-//            FileOutputStream fos = new FileOutputStream(file.getAbsolutePath() + TIMES + ".gz");
-//            GZIPOutputStream gzos = new GZIPOutputStream(fos)){
-//                indexLock.writeLock().lock();
-//                byte[] bytes = new byte[MAXFILELENTH];
-//                int len;
-//                while ((len = fis.read(bytes)) != -1) {
-//                    gzos.write(bytes, 0, len);
-//                }
-//                ClearDataBaseFile(file.getAbsolutePath());
-//            }
-//            catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//            finally {
-//                indexLock.writeLock().unlock();
-//            }
-        }
     }
 }
